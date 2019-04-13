2e7fc98be6efbced34b1658fa143603c0596405e
LiuZhiTaoGit <193129139@qq.com>
2019-04-13 20:06:00 +0800

    第一次

---
+++ opp/01.py
@@ -0,0 +1,11 @@
+class PythonStudent():
+    name = None
+    age = 13
+    course = "python"
+    def study(self):
+        print("我只爱学习")
+        return None
+
+mingyue = PythonStudent()
+print(PythonStudent.age)
+mingyue.study()
